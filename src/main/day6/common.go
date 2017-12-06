0fc97f8a29bcf05f3014b9005677ef10bb823624
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-12-06 08:36:25 +0100

    day6

---
+++ src/main/day6/common.go
@@ -0,0 +1,34 @@
+package day6
+
+import "strconv"
+
+func Hash(mem []int) string {
+	hash := ""
+	for _, m := range mem {
+		hash = hash + strconv.FormatInt(int64(m), 10) + "#"
+	}
+	return hash
+}
+
+func Modify(mem []int) []int{
+	maxi := MaxIndex(mem)
+	toRedistribute := mem[maxi]
+	mem[maxi] = 0
+	for i:=1; i<=toRedistribute; i++ {
+		mem[(maxi + i)%len(mem)] = mem[(maxi + i)%len(mem)] + 1
+	}
+	return mem
+}
+
+func MaxIndex(mem []int) int {
+	max := -1
+	index := 0
+	for i, x := range mem {
+		if x > max {
+			index = i
+			max = x
+		}
+	}
+	return index
+}
+
---
+++ src/main/day6b.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"log"
+	"io/ioutil"
+	"strings"
+	"strconv"
+	"fmt"
+	"main/day6"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("can't open file", err)
+	}
+
+	b, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal("couldn't read file", err)
+	}
+
+	input := string(b)
+	mems := strings.Split(strings.TrimSpace(input), "\t")
+
+	mem := make([]int, len(mems))
+	for i, m := range mems {
+		mem[i], err = strconv.Atoi(m)
+		if err != nil {
+			log.Fatal("couldn't convert number", m, err)
+		}
+	}
+
+	seen := make(map[string]int)
+
+	for i := 0; ; i++ {
+		x, ok := whereSeen(mem, seen)
+		if ok {
+			fmt.Println(i-x)
+			break
+		}
+		addIndex(mem, seen, i)
+		mem = day6.Modify(mem)
+	}
+}
+
+func addIndex(mem []int, seen map[string]int, iteration int) {
+	seen[day6.Hash(mem)] = iteration
+}
+
+func whereSeen(mem []int, seen map[string]int) (int, bool) {
+	x, ok := seen[day6.Hash(mem)]
+	return x, ok
+}
