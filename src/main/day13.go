b071f8b1fd1a7ca76b376acd53a90c7d99137ccd
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-12-13 09:14:04 +0100

    day13

---
+++ src/main/day13.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"log"
+	"bufio"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("can't open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	firewall := make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := strings.Split(strings.TrimSpace(line), ": ")
+		layer, _ := strconv.Atoi(tokens[0])
+		level, _ := strconv.Atoi(tokens[1])
+		firewall[layer] = level
+	}
+
+	severity := 0
+	for layer, level := range firewall {
+		if layer % ((level - 1)*2) == 0 {
+			severity = severity + layer*level
+		}
+	}
+
+	fmt.Println(severity)
+}
---
+++ src/main/day13b.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+	"log"
+	"bufio"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
+func main() {
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("can't open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	firewall := make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := strings.Split(strings.TrimSpace(line), ": ")
+		layer, _ := strconv.Atoi(tokens[0])
+		level, _ := strconv.Atoi(tokens[1])
+		firewall[layer] = level
+	}
+
+	delay := 0
+	for ; ; delay++ {
+		severity := 0
+		for layer, level := range firewall {
+			if (layer+delay)%((level-1)*2) == 0 {
+				severity = severity + 1
+			}
+		}
+		if severity == 0 {
+			fmt.Println("found it")
+			break
+		}
+	}
+
+	fmt.Println(delay)
+}
