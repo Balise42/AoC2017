46e24a222f0118c45242ee49615cb40251a46107
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-12-15 07:03:48 +0100

    day15. This is not very satisfying, perf-wise. But my computer is less lazy than I am.

---
+++ src/main/day15.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	gena := int64(16807)
+	genb := int64(48271)
+	mod := int64(2147483647)
+
+	a := int64(289)
+	b := int64(629)
+
+	tot := 0
+
+	for i := 0; i<40000000; i++ {
+		a = (gena * a) % mod
+		b = (genb * b) % mod
+		abin := strconv.FormatInt(a, 2)
+		bbin := strconv.FormatInt(b, 2)
+		diff := false
+		for j := 1; j<=16; j++ {
+			if abin[len(abin) - j] != bbin[len(bbin) - j] {
+				diff = true
+				break
+			}
+		}
+		if !diff {
+			tot = tot + 1
+		}
+	}
+	fmt.Println(tot)
+}
