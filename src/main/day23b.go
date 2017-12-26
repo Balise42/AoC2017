38f3e92c3a98db45311ace4139d30bcb2c66bcf7
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-12-26 17:41:27 +0100

    ugly bruteforce. eh.

--- src/main/day23b.go
+++ src/main/day23b.go
@@ -3,60 +3,60 @@ package main
 import "fmt"
 
 func main() {
-	a := 1
+
 	b := 81
 	c := 81
 	h := 0
-	if a != 0 {
-		goto L1
-	}
-	goto start
-L1:
+
 	b = b * 100
 	b = b + 100000
 	c = b
 	c = c + 17000
-start:
-	f := 1
-	d := 2
-L5:
-	e := 2
+	i := 0
+	for {
+		i++
+		f := 1
+		d := 2
 
-L4:
-	g := d
-	g = g * e
-	g = g - b
-	if g != 0 {
-		goto L3
+		g := 1
+		e:=2
+		for g != 0 {
+			e = 2
+
+			for g != 0 {
+				g = d
+				g = g * e
+				g = g - b
+				if g == 0 {
+					f = 0
+					goto L
+				}
+
+				e = e + 1
+				g = e
+				g = g - b
+			}
+			d = d + 1
+			g = d
+			g = g - b
+		}
+		fmt.Println(d)
+		L: if f == 0 {
+			h = h + 1
+			fmt.Println(i, "inc")
+		} else {
+			fmt.Println(i, "nop")
+		}
+
+		g = b
+		g = g - c
+		if g == 0 {
+			break
+		}
+		fmt.Println(h, g, b, c)
+
+		b = b + 17
 	}
-	f = 0
-L3:
-	e = e + 1
-	g = e
-	g = g - b
-	if g != 0 {
-		goto L4
-	}
-	d = d + 1
-	g = d
-	g = g - b
-	if g != 0 {
-		goto L5
-	}
-	if f != 0 {
-		goto L6
-	}
-	h = h + 1
-L6:
-	g = b
-	g = g - c
-	if g != 0 {
-		goto L7
-	}
-	goto end
-L7:
-	b = b + 17
-	goto start
-end:
+
 	fmt.Println(h)
 }
