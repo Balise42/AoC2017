822c91e329b914e5d66b4433ef466aeb1be0383a
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2017-12-26 10:31:10 +0100

    backup

--- src/main/day23b.go
+++ src/main/day23b.go
@@ -1,76 +1,48 @@
 package main
 
-import (
-	"os"
-	"log"
-	"bufio"
-	"strings"
-	"strconv"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	filename := os.Args[1]
-	file, err := os.Open(filename)
-	defer file.Close()
-
-	if err != nil {
-		log.Fatal("can't open file", err)
-	}
-
-	program := make([][]string, 0, 0)
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		program = append(program, strings.Split(strings.TrimSpace(scanner.Text()), " "))
-	}
-
-	registers := make(map[string]int)
-	registers["a"] = 1
-	counter := 0
+	b := 108100
+	c := 125100
+	h := 0
 
 	for {
-		if counter >= len(program) {
+		f := 1
+		d := 2
+		g := 2
+
+		for g != 0 {
+			e := 2
+
+			for g != 0 {
+				g = d
+				g = g * e
+				g = g - b
+
+				if g != 0 {
+					f = 0
+				}
+
+				e = e + 1
+				g = e
+				g = g - b
+			}
+
+			d = d + 1
+			g = d
+			g = g - b
+		}
+
+		if f != 0 {
+			h = h + 1
+		}
+		g = b
+		g = g - c
+		if g == 0 {
 			break
 		}
-		instruction := program[counter]
-
-		if instruction[0] == "set" {
-			val, err := strconv.Atoi(instruction[2])
-			if err != nil {
-				registers[instruction[1]] = registers[instruction[2]]
-			} else {
-				registers[instruction[1]] = val
-			}
-		} else if instruction[0] == "mul" {
-			val, err := strconv.Atoi(instruction[2])
-			if err != nil {
-				registers[instruction[1]] = registers[instruction[1]] * registers[instruction[2]]
-			} else {
-				registers[instruction[1]] = registers[instruction[1]] * val
-			}
-		} else if instruction[0] == "sub" {
-			val, err := strconv.Atoi(instruction[2])
-			if err != nil {
-				registers[instruction[1]] = registers[instruction[1]] - registers[instruction[2]]
-			} else {
-				registers[instruction[1]] = registers[instruction[1]] - val
-			}
-		} else if instruction[0] == "jnz" {
-			val, err := strconv.Atoi(instruction[1])
-			if err != nil {
-				val = registers[instruction[1]]
-			}
-			if val != 0 {
-				skip, err := strconv.Atoi(instruction[2])
-				if err != nil {
-					skip = registers[instruction[2]]
-				}
-				counter = counter + skip
-				continue
-			}
-		}
-		counter = counter + 1
+		b = b + 17
 	}
-
-	fmt.Println(registers["h"])
+	fmt.Println(h)
 }
